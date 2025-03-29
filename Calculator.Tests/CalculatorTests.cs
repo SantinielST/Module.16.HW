f3d3f319b650d6ea9081059823f9eb2351786246
Name <email@example.ru>
2025-03-29 11:59:14 +0300

    Add AdditionalMustReturnCorrectValue

---
+++ Calculator.Tests/CalculatorTests.cs
@@ -0,0 +1,15 @@
+﻿using Module._16.HW;
+
+namespace CalculatorTests;
+
+[TestFixture]
+public class CalculatorTests
+{
+    public Calculator calculator = new Calculator();
+
+    [Test]
+    public void AdditionalMustReturnCorrectValue()
+    {
+        Assert.That(4, Is.EqualTo(calculator.Additional(2, 2)));
+    }
+}
