ad26e012ddae9bc96803cf3b0df179923d765be5
Name <email@example.ru>
2025-03-29 12:13:49 +0300

    Additional checks added

--- Calculator.Tests/CalculatorTests.cs
+++ Calculator.Tests/CalculatorTests.cs
@@ -8,31 +8,37 @@ public class CalculatorTests
     public Calculator calculator = new Calculator();
 
     [Test]
-    public void AdditionalMustReturnCorrectValue()
+    public void AdditionalMustReturnCorrectValue() // Проверяем корректность работы метода сложения
     {
         Assert.That(4, Is.EqualTo(calculator.Additional(2, 2)));
+        Assert.That(8, Is.EqualTo(calculator.Additional(5, 3)));
     }
 
     [Test]
-    public void SubtractionMustReturnCorrectValue()
+    public void SubtractionMustReturnCorrectValue() // Проверяем корректность работы метода вычитания
     {
         Assert.That(0, Is.EqualTo(calculator.Subtraction(2, 2)));
+        Assert.That(-4, Is.EqualTo(calculator.Subtraction(2, 6)));
     }
 
     [Test]
-    public void MiltiplicationMustReturnCorrectValue()
+    public void MiltiplicationMustReturnCorrectValue() // Проверяем корректность работы метода умножения
     {
         Assert.That(4, Is.EqualTo(calculator.Miltiplication(2, 2)));
+        Assert.That(20, Is.EqualTo(calculator.Miltiplication(2, 10)));
     }
 
     [Test]
-    public void DivisionMustReturnCorrectValue()
+    public void DivisionMustReturnCorrectValue() // // Проверяем корректность работы метода деления
     {
         Assert.That(1, Is.EqualTo(calculator.Division(2, 2)));
+        Assert.That(12, Is.EqualTo(calculator.Division(60, 5)));
+        Assert.That(3, Is.EqualTo(calculator.Division(7, 2)));
+        Assert.That(0, Is.EqualTo(calculator.Division(7, 8)));
     }
 
     [Test]
-    public void DivisionMustThrowException()
+    public void DivisionMustThrowException() // Деление на 0 должно вызвать ошибку
     {
         Assert.Throws<DivideByZeroException>(() => calculator.Division(2, 0));
     }
--- Module.16.HW/Calculator.cs
+++ Module.16.HW/Calculator.cs
@@ -2,22 +2,22 @@
 
 public class Calculator
 {
-    public int Additional(int a, int b)
+    public int Additional(int a, int b) // Метод сложения
     {
         return a + b;
     }
 
-    public int Subtraction(int a, int b)
+    public int Subtraction(int a, int b) // Метод вычитания
     {
         return a - b;
     }
 
-    public int Miltiplication(int a, int b)
+    public int Miltiplication(int a, int b) // Метод умножения
     {
         return a * b;
     }
 
-    public int Division(int a, int b)
+    public int Division(int a, int b) // Метод деления
     {
         return a / b;
     }
